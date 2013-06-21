717b4b7684afcb5e550b8c149b22e9c61624a1f8
Sho Hashimoto <hashimoto@shokai.org>
2013-06-21 12:21:22 +0900

    watch ["notifications"] and tweet babascript code

---
+++ linda-babascript-notify-twitter.rb
@@ -0,0 +1,38 @@
+#!/usr/bin/env ruby
+require 'rubygems'
+require 'em-rocketio-linda-client'
+$stdout.sync = true
+
+url   = ENV["LINDA_BASE"]  || ARGV.shift || "http://localhost:5000"
+space = ENV["LINDA_SPACE"] || "takumibaba"
+space_tweet = ENV["LINDA_SPACE_TWEET"] || "test"
+puts "connecting.. #{url}"
+
+EM::run do
+  linda = EM::RocketIO::Linda::Client.new url
+  ts = linda.tuplespace[space]
+  ts_tweet = linda.tuplespace[space_tweet]
+
+  linda.io.on :connect do  ## RocketIO's "connect" event
+    puts "RocketIO connect!! (#{linda.io.type})"
+    ts.watch ["notifications"] do |tuple|
+      p tuple
+      next if tuple.size < 2
+      tuple.shift
+      code = tuple.shift.to_s
+      args = tuple.shift
+      if args.kind_of? Array and !args.empty?
+        args = args.map{|i| %Q{"#{i}"} }.join(', ')
+      end
+      puts code += "(#{args})"
+      tuple = ["twitter", "tweet", "baba -e #{code}"]
+      p tuple
+      ts_tweet.write tuple
+    end
+  end
+
+  linda.io.on :disconnect do
+    puts "RocketIO disconnected.."
+  end
+
+end
