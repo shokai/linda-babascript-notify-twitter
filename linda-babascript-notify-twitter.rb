85bb38de0a3e686a8c6102e49729a59eec4e4eb9
Sho Hashimoto <hashimoto@shokai.org>
2013-06-25 17:30:01 +0900

    watch ["babascript", "eval"]

--- linda-babascript-notify-twitter.rb
+++ linda-babascript-notify-twitter.rb
@@ -15,11 +15,11 @@ EM::run do
 
   linda.io.on :connect do  ## RocketIO's "connect" event
     puts "RocketIO connect!! (#{linda.io.type})"
-    ts.watch ["babascript"] do |tuple|
+    ts.watch ["babascript","eval"] do |tuple|
       p tuple
       next if tuple.size < 3
       tuple.shift
-      code = "#{tuple.shift} #{tuple.shift}"
+      code = tuple.shift.to_s
       args = tuple.shift
       if args.kind_of? Array and !args.empty?
         args = args.map{|i| %Q{"#{i}"} }.join(', ')
